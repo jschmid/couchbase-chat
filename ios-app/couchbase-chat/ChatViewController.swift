81d35eca99f893cbd69bded7326130a481a74d6e
Jonas Schmid <jonas.schmid@gmail.com>
2015-07-10 17:17:44 +0200

    Working chat with channels

--- ios-app/couchbase-chat/ChatViewController.swift
+++ ios-app/couchbase-chat/ChatViewController.swift
@@ -12,16 +12,19 @@ class ChatViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
 
+    @IBOutlet weak var textField: UITextField!
     private var liveQuery: CBLLiveQuery?
 
     var database: CBLDatabase = {
         let app = UIApplication.sharedApplication().delegate as! AppDelegate
         let db = app.database
 
-        db.viewNamed("messages").setMapBlock("1") { (doc, emit) in
+        db.viewNamed("messages").setMapBlock("2") { (doc, emit) in
             if let type = doc["type"] as? String where type == "message" {
-                if let room = doc["room"] as? String, let date = doc["created_at"] as? String {
-                    emit([room, date], nil)
+                if let room = doc["room"] as? String,
+                    let date = doc["created_at"] as? String,
+                    let message = doc["message"] as? String {
+                    emit([room, date], message)
                 }
             }
         }
@@ -39,19 +42,25 @@ class ChatViewController: UIViewController {
     func configureView() {
         if let chatroom = self.chatroom {
             self.title = chatroom
+
+            if let live = liveQuery {
+                live.removeObserver(self, forKeyPath: "rows")
+            }
+
+            let view = database.viewNamed("messages")
+            let query = view.createQuery()
+            query.startKey = [self.chatroom!]
+            query.endKey = [self.chatroom!, [:]]
+            let live = query.asLiveQuery()
+
+            live.addObserver(self, forKeyPath: "rows", options: [], context: nil)
+
+            liveQuery = live
         }
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        let view = database.viewNamed("messages")
-        let query = view.createQuery()
-        let live = query.asLiveQuery()
-
-        live.addObserver(self, forKeyPath: "rows", options: [], context: nil)
-
-        liveQuery = live
     }
 
     deinit {
@@ -60,6 +69,28 @@ class ChatViewController: UIViewController {
 
     override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [NSObject : AnyObject]?, context: UnsafeMutablePointer<Void>) {
         self.tableView.reloadData()
+    }
+
+    @IBAction func sendClick(sender: UIButton) {
+        let text = textField.text
+
+        if text == nil || text?.lengthOfBytesUsingEncoding(NSUTF8StringEncoding) <= 0 {
+            return
+        }
+
+        let message = text!
+
+        let properties = [
+            "type": "message",
+            "room": self.chatroom!,
+            "created_at": CBLJSON.JSONObjectWithDate(NSDate()),
+            "message": message
+        ];
+
+        let doc = database.createDocument()
+        try! doc.putProperties(properties)
+
+        textField.text = ""
     }
 
     func numberOfSectionsInTableView(tableView: UITableView) -> Int {
@@ -77,32 +108,23 @@ class ChatViewController: UIViewController {
     }
 
     func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+        let cellIdentifier = "messageCell"
 
-        if let row = liveQuery?.rows?.rowAtIndex(UInt(indexPath.row)),
-            let name = row.key as? String {
-                cell.textLabel?.text = name
+        var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as UITableViewCell!
+
+        if cell == nil {
+            cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: cellIdentifier)
         }
-        
-        return cell
-    }
 
-//    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-//        let cellIdentifier = "messageCell"
-//
-//        var cell = tableView.dequeueReusableCellWithIdentifier(cellIdentifier) as UITableViewCell!
-//
-//        if cell == nil {
-//            cell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: cellIdentifier)
-//        }
-//
-//        if let cell = cell {
-//            cell.textLabel!.text = "Yeah"
-//            cell.detailTextLabel?.text = "ok"
-//        }
-//
-//        return cell
-//
-//    }
+        if let cell = cell,
+            let row = liveQuery?.rows?.rowAtIndex(UInt(indexPath.row)),
+            let message = row.value as? String {
+
+            cell.textLabel!.text = message
+        }
+
+        return cell
+
+    }
 
 }

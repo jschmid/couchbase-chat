ea033e39ef91361a56ba563772a14d634eb07e87
Jonas Schmid <jonas.schmid@gmail.com>
2015-07-16 07:57:31 +0200

    Fixed Core Data + Swift 2

--- ios-app/couchbase-chat/Model/Chatroom.swift
+++ ios-app/couchbase-chat/Model/Chatroom.swift
@@ -9,7 +9,6 @@
 import Foundation
 import CoreData
 
-@objc(Chatroom)
 class Chatroom: NSManagedObject {
 
 // Insert code here to add functionality to your managed object subclass
--- ios-app/couchbase-chat/Model/Message.swift
+++ ios-app/couchbase-chat/Model/Message.swift
@@ -9,7 +9,6 @@
 import Foundation
 import CoreData
 
-@objc(Message)
 class Message: NSManagedObject {
 
 // Insert code here to add functionality to your managed object subclass
